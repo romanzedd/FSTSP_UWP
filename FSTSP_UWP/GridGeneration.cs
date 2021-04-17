6c58782459b69f87f277cf6a53ac26962ded6459
Pavel Silchenkov <pavel.silchenkov13@gmail.com>
2021-04-17 22:22:16 +0200

    chore: remove unused usings

--- FSTSP_UWP/GridGeneration.cs
+++ FSTSP_UWP/GridGeneration.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
+using System.Linq;
 using System.Xml;
 using System.Xml.Linq;
-using System.Xml.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace FSTSP_UWP
--- FSTSP_UWP/Space/LoadData.cs
+++ FSTSP_UWP/Space/LoadData.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 //using System.Windows.Forms;
-using System.IO;
-using System.Security.Cryptography;
 
 namespace FSTSP_UWP
 {
